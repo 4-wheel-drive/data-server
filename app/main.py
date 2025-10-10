903f758e32e5cee8838f8d99e3b998fbb7bcd6f4
JunhoKim <154494090+Mr-Tongyoung@users.noreply.github.com>
2025-10-10 13:24:45 +0900

    fix: remove 'app.' prefix in imports

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from app.schedule.web_socket_token_scheduler import start_scheduler, shutdown_scheduler
-from app.schedule.access_token_scheduler import start_access_token_scheduler, shutdown_access_token_scheduler
-from app.domain.minute_quotes.quotes_service import start_quotes, stop_quotes
-from app.schedule.daily_quotes_scheduler import start_daily_quotes_scheduler, shutdown_daily_quotes_scheduler
-from app.domain.minute_quotes.mock_ws_client import mock_subscribe
+from schedule.web_socket_token_scheduler import start_scheduler, shutdown_scheduler
+from schedule.access_token_scheduler import start_access_token_scheduler, shutdown_access_token_scheduler
+from domain.minute_quotes.quotes_service import start_quotes, stop_quotes
+from schedule.daily_quotes_scheduler import start_daily_quotes_scheduler, shutdown_daily_quotes_scheduler
+from domain.minute_quotes.mock_ws_client import mock_subscribe
 
 
 @asynccontextmanager
