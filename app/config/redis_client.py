a3b1fce001b57302475b0022586ee256c230c6e2
JunhoKim <154494090+Mr-Tongyoung@users.noreply.github.com>
2025-10-10 14:22:31 +0900

    Update redis_client.py

--- app/config/redis_client.py
+++ app/config/redis_client.py
@@ -1,7 +1,7 @@
 import redis
 import os
 
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_HOST = os.getenv("REDIS_HOST", "redis.kafka")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_DB = int(os.getenv("REDIS_DB", 0))
 
